bbc2e7001f6ad2beb7b733e8481f454eb5e912db
redricasa <fredeb26@gmail.com>
2019-08-10 23:17:51 -0700

    added key press event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,55 @@
 // - declare variables for computer guess and user guess
-  // declare a variable for computerChoice set
-    //  to an array of all the English alphabets a-z
-    var compChoice= ['a','b','c','d','e','f','g', 'h','i','j','k','l','m',
-    'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// declare a variable for computerChoice set
+//  to an array of all the English alphabets a-z
+var compChoice= ['a','b','c','d','e','f','g', 'h','i','j','k','l','m',
+'n','o','p','q','r','s','t','u','v','w','x','y','z'];
 // declare a variable COMPUTER guess to be set to computer choices which hold the function of the random number generator
 //  -- Math.floor(Math.random() * 28) //-- 28 since the computer will pick b/n 1 and 27- # of letters in the alphabet.
-        // var compGuess = compChoice[Math.floor(Math.random() * 28)];
-        // console.log(compGuess)
+// var compGuess = compChoice[Math.floor(Math.random() * 28)];
+// console.log(compGuess)
 
-        function chooseLetter(array) {
-            //using 'array' in the argument in order to use the function again with other arrays
-            console.log (array[Math.floor(Math.random() * compChoice.length)]);
-            // choose a random letter from array
-        }
-        var compLetter = chooseLetter(); //declared a variable to run the function
-        console.log('Comp Letter:', compLetter);// console loggs out compChoice   
+function chooseLetter(array) {
+//using 'array' in the argument in order to use the function again with other arrays
+  var arrayLength=array.length;
+  var randomDecimal=Math.random();//returns a random # b/n 0.01 and 0.99
+  var decimalIndex= arrayLength *randomDecimal;//changes 10^1 from 10^-1;meaning 0.26 to 25.74
+  var index=Math.floor(decimalIndex);//changes from 0 to 25
+  var letter=array[index]; //access the letter at the index
+  return letter;
 
-         // var userGuess --
+// return (array[Math.floor(Math.random() * array.length)]);
+// choose a random letter from array
+}
+var compLetter = chooseLetter(compChoice);
+//declared a variable to run the function
+console.log('Comp Letter:', compLetter);// console loggs out compChoice   
+
+// var userGuess --
 // // - use onkeypress event type for USER guess function  
+// document.addEventListener("keypress", function(event){
+//   console.log(event);
+// });
+var guesses = 10;
+var win = 0;
+var loss = 0;
+function onkeypress(event){
+  var guessedKey = event.key;
+  if (guessedKey === compLetter){
+    win++;
+  }
+  else{
+    loss++;
+  }
+}
+//to limit # of guesses to 25, use a for loop
+
+
+
+
+// reset game by getting a new compLetter without refreshing the page
+
+
+
 //     document.getElementById("guessesSoFar").onkeypress = function() {userGuessFunction()};
 
 //     // declare a function for computer guess and user guess
